017881d04c67241c113622c2d250a69011e870bf
HwangSeungHyeon <xlddy02@gmail.com>
2024-01-04 09:41:27 +0900

    ADD: "add the answer code for question 50"
    5월 식품들의 총매출을 조회하는 sql 문제 풀이

---
+++ level4/50_5월 식품들의 총매출 조회하기.sql
@@ -0,0 +1,11 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/131117
+SELECT
+    p.product_id,
+    product_name,
+    SUM(price * amount) total_sales
+FROM food_product p
+    INNER JOIN food_order o
+    ON p.product_id = o.product_id
+WHERE DATE_FORMAT(produce_date, "%Y-%m") = "2022-05"
+GROUP BY product_id
+ORDER BY total_sales DESC, p.product_id
