4a1087e5dd88e0f2c3aecda14c9ad89c8f36dad2
HwangSeungHyeon <xlddy02@gmail.com>
2024-01-04 09:18:08 +0900

    ADD: "add the answer code for question 48"
    즐겨찾기가 가장 많은 식당 정보 출력하는 sql 문제 풀이

---
+++ level3/48_즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,8 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/131123
+SELECT food_type, rest_id, rest_name, favorites
+FROM rest_info
+WHERE favorites IN (SELECT MAX(favorites)
+                  FROM rest_info
+                  GROUP BY food_type)
+GROUP BY food_type
+ORDER BY food_type DESC
