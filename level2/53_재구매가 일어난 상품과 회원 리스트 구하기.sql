1698325123317057899f5fe82657997ba5287c6c
HwangSeungHyeon <xlddy02@gmail.com>
2024-01-05 14:17:18 +0900

    ADD: "add the answer code for question 53"
    재구매가 일어난 상품과 회원 리스트 구하는 sql 문제 풀이

---
+++ level2/53_재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,6 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/131536
+SELECT user_id, product_id
+FROM online_sale
+GROUP BY user_id, product_id
+HAVING count(*) > 1
+ORDER BY user_id, product_id DESC
